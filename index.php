a308a37f38c35a4cf679cc5d5daa3768389509f9
Eliadelante <eliana77lopera@gmail.com>
2016-08-20 13:33:55 -0500

    Update index.php
    quite asteriscos

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 /**
  * CodeIgniter
- *
+ 
  * An open source application development framework for PHP
- *
+ 
  * This content is released under the MIT License (MIT)
- *
+ 
  * Copyright (c) 2014 - 2016, British Columbia Institute of Technology
- *
+ 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -93,7 +93,7 @@ switch (ENVIRONMENT)
  *---------------------------------------------------------------
  * SYSTEM DIRECTORY NAME
  *---------------------------------------------------------------
- *
+ 
  * This variable must contain the name of your "system" directory.
  * Set the path if it is not in the same directory as this file.
  */
